18fc4f67bb2aabde9c093222a170f9885573c3da
SophieTopart <sophiebailly9@gmail.com>
2021-04-14 12:34:07 +0200

    Finalisation Header

--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const Mainpage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    return <div></div>
+}
 
-export default Mainpage;
+export default Mainpage
