d871867b389a62a77f0c3e6685e4e56a170e618b
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-04-15 21:54:18 +0200

    improvements to CSS icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,24 +6,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Footer = () => {
     return (
         <div className="footer">
-            <div>
+            <div className="terms">
                <p>Terms and Conditions</p>
             </div>
             <div className="social-medias">
                 <div>
                     <a href="https://www.facebook.com/Fridgin-Hungry-106454081572173" target="_blank"><FontAwesomeIcon icon={['fab', 'facebook-square']} className="social-icon" /></a>
-                </div>
-                <div>
+                    </div>
+                    <div>
                     <a href="https://twitter.com/FridginH" target="_blank"><FontAwesomeIcon icon={['fab', 'twitter-square']} className="social-icon" /></a>
-                </div>
-                <div>
+                    </div>
+                    <div>
                     <a href="https://www.instagram.com/fridginhungry/" target="_blank"><FontAwesomeIcon icon={['fab', 'instagram-square']} className="social-icon" /></a>
-                </div>
-                <div>
+                    </div>
+                    <div>
                     <a href="https://github.com/Julien-Brcht-Rndlt/fridgin-hungry" target="_blank"><FontAwesomeIcon icon={['fab', 'github-square']} className="social-icon" /></a>
-                </div>
+                    </div>
             </div>
-            <div>
+            <div className="contact">
                <p>Contact us</p>
             </div> 
         </div>
