cb099809eedda4e25e5dbf3441bd52fe2c2fce5d
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-04-15 17:44:19 +0200

    adding Social Medias FonteAwesome icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Footer.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Footer = () => {
     return (
         <div className="footer">
            <p>Terms and Conditions</p>
             <div>
-       
+                { /* <FontAwesomeIcon icon="coffee" /> */ }
+                <FontAwesomeIcon icon={['fab', 'facebook-square']} />
+                <FontAwesomeIcon icon={['fab', 'twitter-square']} />
             </div>
            <div>Contact us</div> 
         </div>
