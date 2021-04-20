2a1c1bfa2d0e7af066abb9a38fe6731a25d0743b
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-04-20 21:59:07 +0200

    add food meal photos

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,6 +6,8 @@ import RecipeResults from '../RecipeResults/RecipeResults'
 const Home = () => {
     const [searchResults, setSearchResults] = useState([])
 
+    const imageSrc = ['/salad-meal.jpg', '/salad-meal-2.png'];
+
     return (
         <div className="home">
             <div className="whitesmoke-bg grid-container">
@@ -15,10 +17,12 @@ const Home = () => {
                     <p>Zero Waste</p>
                     <p>100% Tasty</p>
                 </div>
-                <div></div>
-                <div></div>
+                <div><img src={imageSrc[0]} alt={'first salad meal'} /></div>
+                <div><img src={imageSrc[1]} alt={'second salad meal'} /></div>
+                <div>
                 <div className="emptyFridge">
                     <h2>Empty your fridge!</h2>
+                </div>
                 </div>
                 <div></div>
             </div>
