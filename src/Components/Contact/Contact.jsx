dc5735334d2465b06d6ad765ed5a7c03c1b5946a
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-04-20 12:46:42 +0200

    add nav back link to contact page

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Contact = () => {
+    return(
+        <div>
+            <Link to='/'>Back</Link>
+        </div>
+    );
+}
+
+export default Contact
