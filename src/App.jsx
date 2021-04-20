23b533cb7a8ffcf85de9adbb0c2b91256a126673
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-04-20 12:18:35 +0200

    import main required components for routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
+import RecipeCards from './Components/RecipeCards/RecipeCards'
+import Contact from './Components/Contact/Contact'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
@@ -25,7 +27,7 @@ function App() {
                     <Home />
                 </Route>
                 <Route exact path='/recipes'>
-                    <Cards />
+                    <RecipeCards />
                 </Route>
                 <Route exact path='/contact'>
                     <Contact />
