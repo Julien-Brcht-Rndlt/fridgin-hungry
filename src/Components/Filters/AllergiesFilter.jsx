041b41255f2a37d0c37ab00b7236c8e246ac8738
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-04-22 09:53:39 +0200

    comments removed

--- src/Components/Filters/AllergiesFilter.jsx
+++ src/Components/Filters/AllergiesFilter.jsx
@@ -18,20 +18,19 @@ const AllergiesFilter = () => {
     
       const handleClickAllergy = (e) => {
         
-        // 1. get the value of the checked allergy 
+
         const allergyValue = e.target.value;
         console.log(allergyValue);
        
     
-        // 2. find the corrensponding object in the array. find => returns the right object 
         const checkedAllergy = allergies.find(allergy => {
           if(allergy.value === allergyValue) return true;
           else return false;
         });
-        // 3. toggle selected value (false to true and vice versa)
+       
         checkedAllergy.selected = !checkedAllergy.selected;
         console.log(checkedAllergy);
-        // 4. set the new state (for the UI)
+       
         setAllergies([...allergies])  
       }
     
@@ -46,7 +45,7 @@ const AllergiesFilter = () => {
                   name="health" 
                   value={allergy.value} 
                   checked={allergy.selected} 
-                  onClick={handleClickAllergy}/> {/* check out what the allergy console.log returns */}
+                  onClick={handleClickAllergy}/> 
 
                   <label for={allergy.value}>
                       {allergy.label}
