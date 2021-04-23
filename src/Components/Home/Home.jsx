574c505fa7d71467250695a4d9483b74d6221a6e
JaddN7 <jerome.shaaban@gmail.com>
2021-04-23 09:21:09 +0200

    Tests

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,23 +9,21 @@ import { ReactComponent as NounPlate1 } from './noun_Plate_3498787.svg'
 const Home = () => {
     const [searchResults, setSearchResults] = useState([])
 
-    const imageSrc = ['/salad-meal-1.jpg', '/salad-meal-2.png'];
+    const imageSrc = ['/salad-bowl.png', '/salad-meal-2.png', "/cheese.png"];
 
     return (
         <div className="home  flex-col-container">
-            <div className="top-container"><img src={imageSrc[0]} alt={'second salad meal'} /></div>
             <div className="whitesmoke-bg start-container">
-                <div></div>
+                <div className="top-container"><img src={imageSrc[0]} alt={'second salad meal'} /></div>
                 <div className="empty-fridge flex-col-container">
                     <h2>Empty you Fridge!</h2>
                     <div className="description flex-col-container">
                         <p>Great recipe ideas</p>
                         <p>Zero Waste</p>
                         <p>100% Tasty</p>
-                    </div> 
+                    </div>
                 </div>
-                <div>
-                </div>               
+                <div className="middle-container"><img src={imageSrc[2]} alt={'second salad meal'} /></div>               
             </div>
 
             <div className="beige-bg">
