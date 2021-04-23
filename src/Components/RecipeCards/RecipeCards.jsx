15c82d9382788cb444293c4fa3a87cb6cf56f1be
JaddN7 <jerome.shaaban@gmail.com>
2021-04-23 12:19:59 +0200

    Recreating css and links for RecipeCards and RecipeCard

--- src/Components/RecipeCards/RecipeCards.jsx
+++ src/Components/RecipeCards/RecipeCards.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
 import RecipeCard from '../RecipeCard/RecipeCard'
-import './RecipeCards.css'
+import { Link } from 'react-router-dom'
+import "./RecipeCards.css"
 
-const RecipeCards = ({ searchResults, id }) => {
+const RecipeCards = ({ searchResults = [], id }) => {
     console.log(searchResults)
     return (
-        <div className="recipe-cards">
         <div>
-            <Link to='/'><button className="nav-button">Back</button></Link>
-        </div>
-        {searchResults.map((card, index) => (
-                <RecipeCard
-                    key={index}
-                    label={card.recipe.label}
-                    image={card.recipe.image}
-                    ingredients={card.recipe.ingredientLines}
-                />
-            ))}
+            <div>
+                <Link to='/'><button className="nav-button">Back</button></Link>
+            </div>
+            <div className="recipe-cards">
+                {searchResults.map((card, index) => (
+                    <RecipeCard
+                        key={index}
+                        label={card.recipe.label}
+                        image={card.recipe.image}
+                        ingredients={card.recipe.ingredientLines}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
