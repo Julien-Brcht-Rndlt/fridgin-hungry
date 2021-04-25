b1f2068940dc3e122530c8deb950c4f43a36a0bf
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-04-25 22:42:59 +0200

    add some photos to bottom container

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,16 +11,21 @@ import { ReactComponent as NounPlate1 } from './noun_Plate_3498787.svg'
 
 const Home = ({ searchResults, setSearchResults }) => {
 
-    const imageSrc = ['/salad-meal-1.jpg', '/salad-meal-2.png'];
+    const imageSrc = ['/cooking.jpg', 'meal-3.png', 'meal-1.png', 'meal-2.png'] //passer en state si défilement de photos ??
+    const startTitles = ['Empty your Fridge!', 'Fill your Plate :)']
+
+    const [startTitle, setStartTitle] = useState(startTitles[0])
 
     return (
     <div className="home  flex-col-container">
-        <div className="top-container"><img src={imageSrc[0]} alt={'second salad meal'} />
+        <div className="top-container">
         </div>
         <div className="whitesmoke-bg start-container">
             <HashLink to="#filters" scroll={(element) => element.scrollIntoView({behavior: "smooth", block: "center", inline: "center"})}>
                 <div className="empty-fridge flex-col-container">
-                    <h2>Empty you Fridge!</h2>
+                    <div>
+                        <h2>{startTitle}</h2>
+                    </div>
                     <div className="description flex-col-container">
                         <p>Great recipe ideas</p>
                         <p>Zero Waste</p>
@@ -28,12 +33,22 @@ const Home = ({ searchResults, setSearchResults }) => {
                     </div> 
                 </div>
             </HashLink>
-        </div>          
+        </div>
+             
         <div id="filters" className="filters-container beige-bg">
                 <RecipeResults setSearchResults={setSearchResults}/>       
         </div>
         <div className="bottom-container whitesmoke-bg">
-            <img src={imageSrc[1]} alt={'third salad meal'} />
+            <div>
+                <img src={imageSrc[2]} alt={'1st meal sample'} />
+            </div>
+            <div>
+                <img src={imageSrc[3]} alt={'2nd meal sample'} />
+            </div>
+            <div>
+                <img src={imageSrc[1]} alt={'3th meal sample'} />
+            </div>
+            
         </div>
         
     </div>
