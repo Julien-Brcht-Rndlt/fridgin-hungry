bd454138f823610833567dc85f1df65e893af1b1
SophieTopart <sophiebailly9@gmail.com>
2021-04-29 12:06:48 +0200

    Modifs responsive non def

--- src/Components/Home/MealsDisplay.jsx
+++ src/Components/Home/MealsDisplay.jsx
@@ -7,13 +7,13 @@ const MealsDisplay = () => {
 
     return (
         <div className='meals-display'>
-            <div>
-                <img src={mealsImg[0]} alt={'1st meal sample'} className="img-first-child" />
+            <div className="img-first-child">
+                <img src={mealsImg[0]} alt={'1st meal sample'} />
             </div>
-            <div>
+            <div  className="img-second-child">
                 <img src={mealsImg[1]} alt={'2nd meal sample'} />
             </div>
-            <div>
+            <div  className="img-third-child">
                 <img src={mealsImg[2]} alt={'3th meal sample'} />
             </div>
         </div>
