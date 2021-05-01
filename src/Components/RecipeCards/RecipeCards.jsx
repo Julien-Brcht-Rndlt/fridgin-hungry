457348ed2c2b8b8eeb1b11317c734de969528f92
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-05-01 20:13:11 +0200

    closing card detail behaviour

--- src/Components/RecipeCards/RecipeCards.jsx
+++ src/Components/RecipeCards/RecipeCards.jsx
@@ -22,25 +22,26 @@ const RecipeCards = ({ searchResults }) => {
             <div className="recipe-cards">
 
             { showCardDetail &&
-               
-                <RecipeCardDetail
-                    label={cardDetail.recipe.label}
-                    image={cardDetail.recipe.image}
-                    servings={cardDetail.recipe.yield}
-                    totalTime={cardDetail.recipe.totalTime}
-                    calories={(cardDetail.recipe.calories).toFixed(2)}
-                    allergyFilter={(cardDetail.recipe.healthLabels).map((label, i) => <li key={i}>{label}</li>)}
-                    dietLabel={cardDetail.recipe.dietLabels}
-                    cholesterol={(cardDetail.recipe.totalNutrients.CHOLE.quantity).toFixed(2)}
-                    sodium={(cardDetail.recipe.totalNutrients.NA.quantity).toFixed(2)}
-                    fiber={(cardDetail.recipe.totalNutrients.FIBTG.quantity).toFixed(2)}
-                    Sugar={(cardDetail.recipe.totalNutrients.SUGAR.quantity).toFixed(2)}
-                    carbs={(cardDetail.recipe.totalNutrients.CHOCDF.quantity).toFixed(2)}
-                    proteins={(cardDetail.recipe.totalNutrients.PROCNT.quantity).toFixed(2)}
-                    fat={(cardDetail.recipe.totalNutrients.FAT.quantity).toFixed(2)}
-                    saturatedFat={(cardDetail.recipe.totalNutrients.FASAT.quantity).toFixed(2)}
-                    ingredients={(cardDetail.recipe.ingredientLines).map((ingredient, i) => <li key={i}>{ingredient}</li>)} 
-                    url={cardDetail.recipe.url}/>
+               <Link to='/recipes'>
+                    <RecipeCardDetail
+                        label={cardDetail.recipe.label}
+                        image={cardDetail.recipe.image}
+                        servings={cardDetail.recipe.yield}
+                        totalTime={cardDetail.recipe.totalTime}
+                        calories={(cardDetail.recipe.calories).toFixed(2)}
+                        allergyFilter={(cardDetail.recipe.healthLabels).map((label, i) => <li key={i}>{label}</li>)}
+                        dietLabel={cardDetail.recipe.dietLabels}
+                        cholesterol={(cardDetail.recipe.totalNutrients.CHOLE.quantity).toFixed(2)}
+                        sodium={(cardDetail.recipe.totalNutrients.NA.quantity).toFixed(2)}
+                        fiber={(cardDetail.recipe.totalNutrients.FIBTG.quantity).toFixed(2)}
+                        Sugar={(cardDetail.recipe.totalNutrients.SUGAR.quantity).toFixed(2)}
+                        carbs={(cardDetail.recipe.totalNutrients.CHOCDF.quantity).toFixed(2)}
+                        proteins={(cardDetail.recipe.totalNutrients.PROCNT.quantity).toFixed(2)}
+                        fat={(cardDetail.recipe.totalNutrients.FAT.quantity).toFixed(2)}
+                        saturatedFat={(cardDetail.recipe.totalNutrients.FASAT.quantity).toFixed(2)}
+                        ingredients={(cardDetail.recipe.ingredientLines).map((ingredient, i) => <li key={i}>{ingredient}</li>)} 
+                        url={cardDetail.recipe.url}/>
+                </Link>
               
             }
 
