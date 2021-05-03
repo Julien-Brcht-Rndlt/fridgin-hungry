aea72927de0c2a8a0c75af86f80d021f6a18a8df
SophieTopart <sophiebailly9@gmail.com>
2021-05-03 15:52:19 +0200

    About us en cours

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import RecipeCard from '../RecipeCard/RecipeCard'
 
-const ourPics = []
+const ourProfile = [
+    {name: Sophie}
+]
 
 const AboutUs = () => {
     return(
+    <div>
         <div>
             <Link to='/'><button className='nav-button'>Back</button></Link>
-           
+        </div>
+        <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum optio architecto illo rem cupiditate illum facere veniam vitae facilis! Quis nihil suscipit consequatur in aliquid, asperiores ea blanditiis dolorum perferendis? </p>
+        </div>
         <div className='contactCard'>
             <h3 className='contactName'></h3>
             <img src='' alt='' className='contactPic' />
             <p className='servings'> Servings:</p>
             <p className='totalTime'> Total time: min</p>
         </div>
-    
-        </div>
+    </div>
+        
     );
 }
 
