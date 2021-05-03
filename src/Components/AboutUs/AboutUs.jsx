eaf5c81a090f2586d8f9dca59d45cc29e66ff83a
SophieTopart <sophiebailly9@gmail.com>
2021-05-03 21:57:41 +0200

    About us almost done

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -7,6 +7,7 @@ import HashNavButton from '../NavButtons/HashNavButton'
 const ourProfile = [
     {
     contactName: 'Sophie',
+    quote: '',
     picture:'https://drive.google.com/uc?id=1L27qBZlFQqKeWTX_kz3V_nqyF57Zz4ZB',
     gitHub: 'https://github.com/SophieTopart',
     linkedin: 'https://www.linkedin.com/in/sophie-bailly-topart-59a0b893/',
@@ -14,24 +15,27 @@ const ourProfile = [
     },
     {
     contactName: 'Jérôme',
-    picture:'https://drive.google.com/uc?id=1RdBTeZ6_hFRxIhs7Yky-mjL1cue9U8Sl',
+    quote: '',
+    picture:'https://drive.google.com/uc?id=1UJd4Ww-f05LCLEGbMN09W5tR61lQ65lL',
     gitHub: 'https://github.com/JeromeShaaban',
     linkedin: 'https://www.linkedin.com/in/j%C3%A9r%C3%B4me-shaaban/',
-    mail: ''
+    mail: 'jerome.shaaban@gmail.com'
     },
     {
     contactName: 'Sarah',
+    quote: 'Chocolate Lover',
     picture:'https://drive.google.com/uc?id=1Nlm5ClZ1X61XTvhNNF81CiV3AZJplQEJ',
     gitHub: 'https://github.com/Sarahbsn',
     linkedin: 'https://www.linkedin.com/in/sarah-bensouna-b25779152/',
-    mail: ''
+    mail: 'sarah.bensouna@hotmail.com'
     },
     {
     contactName: 'Julien',
+    quote: '',
     picture: 'https://drive.google.com/uc?id=1CilJ4lN_4Fvemuixj9g5OWBrf6ooakQB',
     gitHub: 'https://github.com/Julien-Brcht-Rndlt',
     linkedin: 'https://www.linkedin.com/in/julien-nicolas-brachet-randoulet-229aa2136/',
-    mail: ''
+    mail: 'jn.brachet.randlt@gmail.com'
     }
 
 ]
@@ -41,7 +45,7 @@ const AboutUs = () => {
     return(
     <div>
         <div className='aboutProject'>
-        <p>How did we can about the idea for Fridin' Hungry? Well, first of all, we love food.<br/> But as wilders, we have very little free time on our hands and our fridge is often half empty. <br/>
+        <p>How did we came about the idea for Fridgin' Hungry? Well, first of all, we love food.<br/> But as wilders, we have very little free time on our hands and our fridge is often half empty. <br/>
             This is why we wanted to create an app which would allow us <br/>to find recipes to use up the few ingredients left at our disposal.
         </p>
         </div>
@@ -50,6 +54,7 @@ const AboutUs = () => {
         <div className='contactCard'>
             <img src={profile.picture} alt={profile.contactName} className='contactPic' />
             <h3 className='contactName'>{profile.contactName}</h3>
+            <p>{profile.quote}</p>
             <div className='socialIcons'>
                 <a href={profile.gitHub} target='_blank'><FontAwesomeIcon icon={['fab', 'github-square']} className='socialIcon' /></a>
                 <a href={profile.linkedin} target='_blank'><FontAwesomeIcon icon={['fab', 'linkedin']} className='socialIcon' /></a>
