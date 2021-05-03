e9afe44c9e4dcbcb17bdc9e60408a85cd18da497
SophieTopart <sophiebailly9@gmail.com>
2021-05-03 18:21:29 +0200

    Cards about us part 2

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './AboutUs.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ourProfile = [
     {
     contactName: 'Sophie',
-    picture:'https://drive.google.com/drive/u/1/folders/1c9F7YAbQfT5UOvQKpfxQNeQPLVg970ma'
+    picture:'https://drive.google.com/file/d/1gWtm8pxadwFJsCJF8CMMH9tK-to79c-wiew?usp=sharing',
+    gitHub: 'https://github.com/SophieTopart',
+    linkedin: 'https://www.linkedin.com/in/sophie-bailly-topart-59a0b893/'
     },
-    {contactName: 'Jérôme'},
+    {contactName: 'Jérôme',
+    picture:'../meal-1.png'},
     {contactName: 'Sarah'},
     {contactName: 'Julien'}
 ]
@@ -25,10 +29,15 @@ const AboutUs = () => {
         <div className='contactDisplay'>
         {ourProfile.map((profile) => 
         <div className='contactCard'>
-            <h3 className='contactName'>{profile.contactName}</h3>
             <img src={profile.picture} alt={profile.contactName} className='contactPic' />
+            <h3 className='contactName'>{profile.contactName}</h3>
             <p className='servings'>Links</p>
-        </div>)}
+            <div className='socialIcons'>
+                <a href={profile.gitHub} target='_blank'><FontAwesomeIcon icon={['fab', 'github-square']} className='social-icon' /></a>
+                <a href={profile.linkedin} target='_blank'><FontAwesomeIcon icon={['fab', 'linkedin']} className='social-icon' /></a>
+            </div>
+        </div>
+        )}
         </div>
     </div>
         
