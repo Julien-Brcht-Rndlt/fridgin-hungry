8f1105dd4d89deca3245bc526c21526477641947
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-05-04 19:59:36 +0200

    conflicts fixed

--- src/Components/RecipeCards/RecipeCards.jsx
+++ src/Components/RecipeCards/RecipeCards.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import RecipeCard from '../RecipeCard/RecipeCard'
 import { Link, useParams } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
@@ -8,7 +9,64 @@ import RecipeCardDetail from '../RecipeCardDetail/RecipeCardDetail'
 import CardAnim from './CardAnim'
 import AlertMessage from '../AlertMessage/AlertMessage'
 
-const RecipeCards = ({ match, searchResults }) => {
+
+
+const RecipeCards = ({ searchUrl, searchResults, setSearchResults, setSearchUrl }) => {
+ 
+    const [start, setStart] = useState(0)
+    const [step, setStep] = useState(10)
+
+    // random prep time
+    const prepTimes = [15, 30, 45, 60]
+    const randomPrepTime = () => prepTimes[Math.floor(Math.random() * 4)]
+
+    const updateUrlNext = (url, param, value) => {
+        const startIndex = url.indexOf(param)
+        let endOfParam = url.substr(startIndex, url.length).indexOf('&')
+        endOfParam = endOfParam > -1 ? endOfParam : url.length
+        const urlPart = url.substr(startIndex, endOfParam)
+        url = url.replace(urlPart, `${param}${value}`)
+
+        return url;
+    }
+
+    const onClickHandler = () => {
+
+        // define next step
+        const end = start + step
+        
+        //https://api.edamam.com/search?q=chicken, tomato, cheese&app_id=f604900f&app_key=b523b505a718166bca1753372a51616f&from=40&to=50
+        //searchUrl
+        searchUrl = searchUrl.includes('&from=') ? updateUrlNext(searchUrl, 'from=', start) : `${searchUrl}&from=${start}`
+        searchUrl = searchUrl.includes('&to=') ? updateUrlNext(searchUrl, 'to=', end) : `${searchUrl}&to=${end}`
+
+        setSearchUrl(searchUrl)
+
+        console.log('searchUrl: ' + searchUrl)
+
+        // fetch API with axios
+        axios
+            .get(searchUrl)
+            .then((response) => response.data)
+            .then((data) => {
+                // grab API data + populate our own recipe JS objects
+                const recipeProperties = ['label', 'image', 'yield', 'totalTime', 'calories', 'healthLabels', 'dietLabels', 'totalNutrients', 'ingredientLines', 'url']
+                let recipes = []
+                data.hits.map((recipeData) => {
+                    const recipe = {}
+                    recipeProperties.forEach((property) => {
+                        recipe[property] = recipeData['recipe'][property]
+                    })
+                    recipe.totalTime = recipe.totalTime !== 0 ? recipe.totalTime : randomPrepTime()
+                    recipes = [...recipes, recipe]
+                })
+
+                 // save new recipes searchResults state
+                setSearchResults((prevState) => [...prevState, ...recipes] )
+            })
+        // next start
+        setStart(end)
+    }
 
 const { id } = useParams()
 const cardId = id ? parseInt(id) : undefined
@@ -63,6 +121,10 @@ const cardDetail =  showCardDetail ? searchResults[cardId] : undefined
                 }
         
             </div>
+            <div className='viewmore-container'>
+            <button onClick={onClickHandler} className='viewmore-button'>View More</button>
+            </div>
+           
         </div>
         
     )
