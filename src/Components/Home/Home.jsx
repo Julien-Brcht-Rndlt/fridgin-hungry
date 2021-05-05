e0217e9c7ca1291ea91adb87f03b74ad8924953a
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-05-05 21:56:47 +0200

    add navplate animation (scale)

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,7 @@ import RecipeResults from '../RecipeResults/RecipeResults'
 import FadeSlideshow from './FadeSlideshow'
 import NavPlateButton from './NavPlateButton'
 import MealsDisplay from './MealsDisplay'
+import SwingAnim from './SwingAnim'
 
 const Home = ({ searchUrl, setSearchUrl, setHasToken, setSearchResults }) => {
 
@@ -15,7 +16,9 @@ const Home = ({ searchUrl, setSearchUrl, setHasToken, setSearchResults }) => {
         <div className='home flex-col-container'>
             <FadeSlideshow easing={'ease-in'} duration={2000} />
             <div className='start-container'>
-                <NavPlateButton title={'Empty your Fridge!'} sentences={['Great recipe ideas', 'Zero Waste', '100% Tasty']} target={'filters'} />
+                <SwingAnim scale={0.9} timing={150}>
+                    <NavPlateButton title={'Empty your Fridge!'} sentences={['Great recipe ideas', 'Zero Waste', '100% Tasty']} target={'filters'} />
+                </SwingAnim>
             </div>
 
             <div id='filters' className='filters-container'>
